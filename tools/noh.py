e55169c0be6c5b8a4812db2055d5106fc4518782
Osamu <fujimoto@pokaran.localdomain>
2021-11-10 18:25:30 +0900

    end

--- tools/noh.py
+++ tools/noh.py
@@ -11,60 +11,61 @@ from chacha20poly1305 import ChaCha20Poly1305
 
 
 #def aes_gcm():
-random.seed(12897619287361897)
 
 key= get_random_bytes(32) #
 #HexMyKey= key.hex()
 nonce = os.urandom(12)
 
 def enc():
+    random.seed(12897619287361897)
+    cip = ChaCha20Poly1305(key)
     c= open("cipher.txt","wb")
     f=open("README.md", "rb")
-    key=(111111).to_bytes(32,byteorder="little") #
+    #key=(111111).to_bytes(32,byteorder="little") #
     while True:
-        
-        #rnd=random.randint(1,32)
-        flag=f.read(16)
+        #nonce=get_random_bytes(12)
+        rnd=random.randint(1,32)
+        flag=f.read(rnd)
         if not flag:
             c.close()
             f.close()
             break
-
+        #print(len(flag))
+        
         cipher= AES.new(key, AES.MODE_GCM)
         ciphertext, tag= cipher.encrypt_and_digest(flag)
         enc= cipher.nonce + ciphertext + tag
-        HexEncryptedOriginalMessage= enc.hex()
+        #HexEncryptedOriginalMessage= enc.hex()
         #print(len(enc))
         #exit()
         c.write(enc)
-        print(HexEncryptedOriginalMessage)
+        #print(HexEncryptedOriginalMessage)
     
-        #rnd=random.randint(1,32)
-        #ff=f.read(16)
-        #cip = ChaCha20Poly1305(key)
-        #ciphertext = cip.encrypt(nonce, ff)
+        rnd=random.randint(1,32)
+        ff=f.read(rnd)
+        ciphertext = cip.encrypt(nonce, ff)
         #print(ciphertext.hex())
-        #c.write(ciphertext.hex()+'\n')
-        #plaintext = cip.decrypt(nonce, ciphertext)
-        #print(plaintext)
+        c.write(ciphertext)
 
 
         
 
 def dec():
+    random.seed(12897619287361897)
     c=open("cipher.txt","rb")
     f=open("plane.txt", "wb")
-    key= (111111).to_bytes(32,byteorder="little") #bytes.fromhex(HexMyKey)
+    #key= (111111).to_bytes(32,byteorder="little") #bytes.fromhex(HexMyKey)
+    cip = ChaCha20Poly1305(key)
 
     while True:
-
-        flag=c.read(48)
+        #nonce=get_random_bytes(12)
+        rnd=random.randint(1,32)
+        flag=c.read(rnd+32)
         if not flag:
             f.close()
             c.close()
             break
 
-       
         data = flag #bytes.fromhex(flag)
         cipher = AES.new(key, AES.MODE_GCM, data[:16]) # nonce
         try:
@@ -73,6 +74,10 @@ def dec():
             f.write(dec)
         except ValueError:
             print("Decryption failed")
+        rnd=random.randint(1,32)
+        ciphertext=c.read(rnd+16)
+        plaintext = cip.decrypt(nonce, ciphertext)
+        print(plaintext)
         
 
 enc()
