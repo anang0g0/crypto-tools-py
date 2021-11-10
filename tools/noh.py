4795fe727e22efe4661bce17789792b29137a897
Osamu <fujimoto@pokaran.localdomain>
2021-11-10 20:25:50 +0900

    input

--- tools/noh.py
+++ tools/noh.py
@@ -5,60 +5,72 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.number import long_to_bytes
 import secrets
 import binascii
-import os
+import os,sys
 import random
+import getpass
 from chacha20poly1305 import ChaCha20Poly1305
+from Crypto.Hash import SHA3_256,SHA256,SHA3_512,SHA512
 
 
-#def aes_gcm():
+#
 
-key= get_random_bytes(32) #
-#HexMyKey= key.hex()
-nonce = os.urandom(12)
+nonce = get_random_bytes(12)
+print(nonce)
+#os.urandom(12)
+password = getpass.getpass('password> ')
+password2 = getpass.getpass('confirm> ')
+if password != password2:
+    print('Passwords do not match.')
+    sys.exit(0)
+sha = SHA3_256.new()
+sha.update(password.encode())
+key = sha.digest()
+v=int.from_bytes(key,byteorder="little")
+
+key = get_random_bytes(32) #
+a=int.from_bytes(key,byteorder="little")
+v^=a
+key=v.to_bytes(32,byteorder="little")
 
 def enc():
-    random.seed(12897619287361897)
+    random.seed(128976192873618971)
     cip = ChaCha20Poly1305(key)
     c= open("cipher.txt","wb")
     f=open("README.md", "rb")
-    #key=(111111).to_bytes(32,byteorder="little") #
+
     while True:
-        #nonce=get_random_bytes(12)
         rnd=random.randint(1,32)
         flag=f.read(rnd)
         if not flag:
             c.close()
             f.close()
             break
-        #print(len(flag))
         
         cipher= AES.new(key, AES.MODE_GCM)
         ciphertext, tag= cipher.encrypt_and_digest(flag)
         enc= cipher.nonce + ciphertext + tag
-        #HexEncryptedOriginalMessage= enc.hex()
-        #print(len(enc))
-        #exit()
         c.write(enc)
-        #print(HexEncryptedOriginalMessage)
     
         rnd=random.randint(1,32)
         ff=f.read(rnd)
+        if not ff:
+            c.close()
+            f.close()
+            break
+
         ciphertext = cip.encrypt(nonce, ff)
-        #print(ciphertext.hex())
         c.write(ciphertext)
 
 
         
 
 def dec():
-    random.seed(12897619287361897)
+    random.seed(128976192873618971)
     c=open("cipher.txt","rb")
     f=open("plane.txt", "wb")
-    #key= (111111).to_bytes(32,byteorder="little") #bytes.fromhex(HexMyKey)
     cip = ChaCha20Poly1305(key)
 
     while True:
-        #nonce=get_random_bytes(12)
         rnd=random.randint(1,32)
         flag=c.read(rnd+32)
         if not flag:
@@ -76,7 +88,12 @@ def dec():
             print("Decryption failed")
         rnd=random.randint(1,32)
         ciphertext=c.read(rnd+16)
+        if not ciphertext:
+            f.close()
+            c.close()
+            break
         plaintext = cip.decrypt(nonce, ciphertext)
+        f.write(plaintext)
         print(plaintext)
         
 
